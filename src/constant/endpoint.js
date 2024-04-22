66ba1024dd5a002102f8ebfc9931e17b43f916de
Emmancodex <megamindcodex@gmail.com>
2024-04-22 21:34:39 +0100

    corrected endpoints

--- src/constant/endpoint.js
+++ src/constant/endpoint.js
@@ -1,4 +1,4 @@
-export const cw_endpoint = "http://localhost:3000/api";
-export const socektIo_endpoint = "http://localhost:4000";
-// export const cw_endpoint = "https://cw-endpoint.onrender.com/api";
-// export const socektIo_endpoint = "https://cw-chatapp-endpoint.onrender.com";
+// export const cw_endpoint = "http://localhost:3000/api";
+// export const socektIo_endpoint = "http://localhost:4000";
+export const cw_endpoint = "https://cw-endpoint.onrender.com/api";
+export const socektIo_endpoint = "https://cw-chatapp-endpoint.onrender.com";
