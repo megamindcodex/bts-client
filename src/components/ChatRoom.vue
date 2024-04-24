6c46b7d73f4f8015c685df4d80c217e9c1432ffc
Emmancodex <megamindcodex@gmail.com>
2024-04-24 12:33:45 +0100

    corrected the reciever nameeror

--- src/components/ChatRoom.vue
+++ src/components/ChatRoom.vue
@@ -39,7 +39,7 @@ onMounted(async () => {
     console.log("user is undifined");
   }
 
-  const users = ref([userName, "codex001"]);
+  const users = ref([userName, receiverName]);
   const conversation = await userStore.getChats(users.value);
 
   if (conversation) {
